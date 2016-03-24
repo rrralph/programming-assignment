ce3ff94ef4bae1129c4f5b7560db44cda69470f5
tracer-ics2015 <tracer@njuics.org>
2016-03-24 14:54:04 +0800

    >  compile NEMU 14307130066 ralph Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt20-1+deb8u2 (2016-01-02) i686 GNU/Linux  14:54:04 up  1:39,  1 user,  load average: 0.04, 0.06, 0.05 19a3d8aaf3be6b55a5ed397f51be1f593b3e245

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -80,7 +80,7 @@ static int cmd_x(char *args){
 //	printf("%x\n",(uint32_t)strtoul("0x1000",0,0));
 	char *argsNum=strtok(args," ");
 	//char *desIndex=strtok(NULL," ");
-	printf("%s\n",argsNum);
+	printf("%d\n",(int)strtoul(argsNum,0,0));
 	//printf("%s: \n",desIndex);
 	//printf("%d  %x \n",cnt,atoi(desIndex));
 	//printf("%x  ",swaddr_read(0x100005,4));
